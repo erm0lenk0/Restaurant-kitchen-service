ce52cdae9b51ca792b23dcc4e80be959e0ef96c5
Ermolenko Andrey <f1nd6r@gmail.com>
2025-08-13 12:11:50 +0300

    add models

--- kitchen_core/models.py
+++ kitchen_core/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
+from django.conf import settings
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name', )
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        ordering = ('years_of_experience', )
+
+    def __str__(self):
+        return f"{self.username}: {self.first_name} {self.last_name}"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=7)
+    dish_type = models.ForeignKey(
+        DishType,
+        on_delete=models.CASCADE,
+        related_name="dishes",
+    )
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name', )
+
+
+    def get_absolute_url(self):
+        return reverse("kitchen_core:dish-detail", args=[str(self.id)])
+
+
+
+
+
